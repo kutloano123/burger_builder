dc07ef5fbe39a00ec25cca54a6a6ed6c6f8fc9e3
kutloano123 <kutloano.thoothe@africacode.academy>
2025-07-15 12:11:46 -0700

    Initial commit: burger builder app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function TopBread() {
+  return <div className="top-bread">Top Bread</div>;
+}
+function Tomato() {
+  return <div className="tomato">Tomato</div>;
+}
+function Meat() {
+  return <div className="meat">Meat</div>;
+}
+function Lettuce() {
+  return <div className="lettuce">Lettuce</div>;
+}
+function BaseBread() {
+  return <div className="base-bread">Base Bread</div>;
+}
 
 function App() {
+  const [includeTomato, setIncludeTomato] = useState(true);
+  const [includeMeat, setIncludeMeat] = useState(true);
+  const [includeLettuce, setIncludeLettuce] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1> Build Your Burger</h1>
+
+      <div className="controls">
+        <label>
+          <input
+            type="checkbox"
+            checked={includeTomato}
+            onChange={() => setIncludeTomato(!includeTomato)}
+          />
+          Tomato
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={includeMeat}
+            onChange={() => setIncludeMeat(!includeMeat)}
+          />
+          Meat
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={includeLettuce}
+            onChange={() => setIncludeLettuce(!includeLettuce)}
+          />
+          Lettuce
+        </label>
+      </div>
+
+      <div className="burger">
+        <TopBread />
+        {includeTomato && <Tomato />}
+        {includeMeat && <Meat />}
+        {includeLettuce && <Lettuce />}
+        <BaseBread />
+      </div>
     </div>
   );
 }
